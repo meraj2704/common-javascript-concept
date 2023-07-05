c020362448bf1f22393a5253acfa5b1edda5e1d7
IJE2704 <mh1669101@gmail.com>
2023-07-05 12:23:06 +0600

    common js concept triple

---
+++ triple.js
@@ -0,0 +1,15 @@
+const first =2;
+const second ='2';
+if(first == second) console.log('value is =');
+else console.log( 'value is !=');
+
+if(first === second) console.log('value is =');
+else console.log( 'value is !=');
+
+const first2 = 1;
+const second2 = true;
+if(first2 == second2) console.log('value is =');
+else console.log( 'value is !=');
+
+if(first2 === second2) console.log('value is =');
+else console.log( 'value is !=');
